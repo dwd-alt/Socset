70f085a7742f5f796edf654dd524f3e743213ccb
dwd-alt <iewlewroman85@gmail.com>
2026-03-20 22:48:47 +0700

    Update j.py

--- j.py
+++ j.py
@@ -1,14 +1,1 @@
-from app import app, db, User
 
-with app.app_context():
-    admin = User(
-        username='kildear',
-        email='kildear@kildear.com',
-        display_name='Администратор Kildear',
-        is_admin=True,
-        is_verified=True
-    )
-    admin.set_password('Admin123!')
-    db.session.add(admin)
-    db.session.commit()
-    print("✅ Администратор создан: kildear / Admin123!")
